275b42b41eb031ef96684bb01a2f15afcdc827f3
kevL <k3v1s73n@gmail.com>
2014-07-19 15:47:13 -0600

    elongate statistical Bars

--- src/Basescape/SoldierDeadInfoState.cpp
+++ src/Basescape/SoldierDeadInfoState.cpp
@@ -86,57 +86,57 @@ SoldierDeadInfoState::SoldierDeadInfoState(size_t soldierId)
 
 	_txtTimeUnits	= new Text(120, 9, 6, yPos);
 	_numTimeUnits	= new Text(18, 9, 131, yPos);
-	_barTimeUnits	= new Bar(170, 7, 150, yPos + 1);
+	_barTimeUnits	= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtStamina		= new Text(120, 9, 5, yPos);
 	_numStamina		= new Text(18, 9, 131, yPos);
-	_barStamina		= new Bar(170, 7, 150, yPos + 1);
+	_barStamina		= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtHealth		= new Text(120, 9, 6, yPos);
 	_numHealth		= new Text(18, 9, 131, yPos);
-	_barHealth		= new Bar(170, 7, 150, yPos + 1);
+	_barHealth		= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtBravery		= new Text(120, 9, 6, yPos);
 	_numBravery		= new Text(18, 9, 131, yPos);
-	_barBravery		= new Bar(170, 7, 150, yPos + 1);
+	_barBravery		= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtReactions	= new Text(120, 9, 6, yPos);
 	_numReactions	= new Text(18, 9, 131, yPos);
-	_barReactions	= new Bar(170, 7, 150, yPos + 1);
+	_barReactions	= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtFiring		= new Text(120, 9, 6, yPos);
 	_numFiring		= new Text(18, 9, 131, yPos);
-	_barFiring		= new Bar(170, 7, 150, yPos + 1);
+	_barFiring		= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtThrowing	= new Text(120, 9, 6, yPos);
 	_numThrowing	= new Text(18, 9, 131, yPos);
-	_barThrowing	= new Bar(170, 7, 150, yPos + 1);
+	_barThrowing	= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtMelee		= new Text(120, 9, 6, yPos);
 	_numMelee		= new Text(18, 9, 131, yPos);
-	_barMelee		= new Bar(170, 7, 150, yPos + 1);
+	_barMelee		= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtStrength	= new Text(120, 9, 6, yPos);
 	_numStrength	= new Text(18, 9, 131, yPos);
-	_barStrength	= new Bar(170, 7, 150, yPos + 1);
+	_barStrength	= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtPsiStrength	= new Text(120, 9, 6, yPos);
 	_numPsiStrength	= new Text(18, 9, 131, yPos);
-	_barPsiStrength	= new Bar(170, 7, 150, yPos + 1);
+	_barPsiStrength	= new Bar(234, 7, 150, yPos + 1);
 
 	yPos += step;
 	_txtPsiSkill	= new Text(120, 9, 6, yPos);
 	_numPsiSkill	= new Text(18, 9, 131, yPos);
-	_barPsiSkill	= new Bar(170, 7, 150, yPos + 1);
+	_barPsiSkill	= new Bar(234, 7, 150, yPos + 1);
 
 	setPalette("PAL_BASESCAPE");
 
